aa6d83b950b251a0b267bef7653340c9f0625ac6
eric <eric.tran@alpaca.vn>
2022-08-13 16:18:15 +0700

    update

--- assets/js/lazyload.js
+++ assets/js/lazyload.js
@@ -1,22 +1,16 @@
 const load = (img)=>{
   const url = img.getAttribute('data-src')
-  console.log("url", url)
   img.setAttribute("src", url)
   img.removeAttribute('data-src')
 }
 document.addEventListener("DOMContentLoaded", function () {
   if ("IntersectionObserver" in window) {
     var lazyImages = document.querySelectorAll('[data-src]')
-    console.log("lazyImages", lazyImages);
-    lazyImages.forEach(img=>{
-      console.log("img", img)
-    })
     let lazyImageObserver = new IntersectionObserver(function (
       entries,
       observer
     ) {
       entries.forEach(function (entry) {
-        console.log("entry.isIntersecting", entry.isIntersecting)
         if (entry.isIntersecting) {
           load(entry.target)
           observer.unobserve(entry.target);
